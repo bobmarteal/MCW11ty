6035a0fae723cb0fab11f2115f820ffbf374763c
bob <bobmarteal@gmail.com>
2022-02-02 09:20:25 -0500

    adds postcss autoprefixer cssnano

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/images");
-    eleventyConfig.addPassthroughCopy("./src/js");
-    return {
-        dir: {
-            input: "src",
-            output: "public",
-            data: "_data",
-        },
-    };
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addWatchTarget("./src/sass/");
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      data: "_data",
+    },
+  };
 };
